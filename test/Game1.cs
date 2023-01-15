6c7b65a36d98aea918cadb7807c662a2778bf9cb
Falingunit <84975935+Falingunit@users.noreply.github.com>
2023-01-15 15:57:51 +0530

    Add project files.

---
+++ test/Game1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using test.Physics;
+
+namespace test
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Random random;
+
+        private List<Rigidbody> bodyList;
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            this._graphics.SynchronizeWithVerticalRetrace = true;
+            this._graphics.PreferredBackBufferWidth = 1280;
+            this._graphics.PreferredBackBufferHeight = 720;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            random = new Random();
+
+            this.bodyList = new List<Rigidbody>();
+            int bodyCount = 10;
+
+            for(int i = 0; i < bodyCount; i++)
+            {
+                int type = random.Next(2);
+
+                Rigidbody body = null;
+
+                float x = random.Next(40, 1260-20);
+                float y = random.Next(40, 700-20);
+
+                if (type == (int)ShapeType.Circle)
+                {
+                    if(!Rigidbody.CreateCircle(30f, new Vector2(x, y), 2f, false, 0.5f, out body, out string errmsg))
+                    {
+                        throw new Exception(errmsg);
+                    }
+
+                }
+                else if(type == (int)ShapeType.Box)
+                {
+                    if (!Rigidbody.CreateBox(30f, 30f, new Vector2(x, y), 2f, false, 0.5f, out body, out string errmsg))
+                    {
+                        throw new Exception(errmsg);
+                    }
+                }
+
+                bodyList.Add(body);
+            }
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkBlue);
+
+            _spriteBatch.Begin();
+
+            foreach(var body in bodyList)
+            {
+                Vector2 pos = body.Position;
+                if(body.ShapeType == ShapeType.Circle)
+                {
+                    _spriteBatch.DrawCircle(pos, body.Radius, 26, Color.White);
+                }
+                else if(body.ShapeType == ShapeType.Box)
+                {
+                    _spriteBatch.DrawRectangle(pos.X, pos.Y, body.Width, body.Height, Color.Red);
+                }
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ test/Physics/World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Physics
+{
+    public sealed class World
+    {
+        public static readonly float MinBodySize = 0.01f * 0.01f;
+        public static readonly float MaxBodySize = 64f * 64f;
+
+        public static readonly float MinDensity = 0.5f;
+        public static readonly float MaxDensity = 21.4f;
+
+    }
+}
